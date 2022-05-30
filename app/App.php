c025dc259e1edd372b87aa8714e9f6fd81e40ba2
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2022-05-30 18:29:20 +0400

    Read one file to array (keys are numbers, perhaps strings are needed).

--- app/App.php
+++ app/App.php
@@ -2,4 +2,23 @@
 
 declare(strict_types = 1);
 
-// Your Code
+
+
+function read_csv_file(string $fileName): array {
+    $data = [];
+
+    if (($handle = fopen($fileName, "r")) !== false) {
+        $keys = get_line_csv($handle);
+        while (($line = get_line_csv($handle)) !== false) {
+            $data[] = $line;
+        }
+        fclose($handle);
+    }
+
+    return $data;
+}
+
+function get_line_csv($stream): array|false {
+    return fgetcsv($stream, 1000, ",");
+}
+
